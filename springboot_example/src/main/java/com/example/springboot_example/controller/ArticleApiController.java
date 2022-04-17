eba998a291a3c50e879d488d9be6e2327ac7773a
Caumannerman <uigohiiming2@gmail.com>
2022-04-17 17:52:04 +0900

    ApiController에 getAllArticles(), getOneArticle() 메서드 추가 ( @GetMapping)

--- springboot_example/src/main/java/com/example/springboot_example/controller/ArticleApiController.java
+++ springboot_example/src/main/java/com/example/springboot_example/controller/ArticleApiController.java
@@ -1,10 +1,31 @@
 package com.example.springboot_example.controller;
 
 
+import com.example.springboot_example.Entity.Article;
+import com.example.springboot_example.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // RestAPI용 컨트롤러! Json을 반환
+import java.util.List;
+
+@RestController // RestAPI용 컨트롤러! 데이터(Json)을 반환
 public class ArticleApiController {
 
-    
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    //GET
+    @GetMapping("/api/articles")
+    public List<Article> getAllArticles(){
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article getOneArticle(@PathVariable Long id){
+        return articleRepository.findById(id).orElse(null);
+    }
+
+
 }
