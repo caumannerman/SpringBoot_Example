627cb3dfd9c1c79e653de5a1551e80c3193973e7
Caumannerman <uigohiiming2@gmail.com>
2022-04-17 17:44:26 +0900

    Article RestController 생성

---
+++ springboot_example/src/main/java/com/example/springboot_example/controller/ArticleApiController.java
@@ -0,0 +1,10 @@
+package com.example.springboot_example.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestAPI용 컨트롤러! Json을 반환
+public class ArticleApiController {
+
+    
+}
