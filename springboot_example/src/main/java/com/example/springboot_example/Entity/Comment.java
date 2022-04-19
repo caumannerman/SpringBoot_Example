5bc93506dab1adc4a8ac23f63749ca5c97944304
Caumannerman <uigohiiming2@gmail.com>
2022-04-19 17:08:33 +0900

    테스트를 위한 article, comment 더미데이터 data.sql에 추가

--- springboot_example/src/main/java/com/example/springboot_example/Entity/Comment.java
+++ springboot_example/src/main/java/com/example/springboot_example/Entity/Comment.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity //새로운 댓글 테이블이 만들어진다!!
 @AllArgsConstructor
@@ -14,4 +14,18 @@ import javax.persistence.Entity;
 @Getter
 @Setter
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne // 해당 댓글 엔티티 여러개가 하나의 Article에 연관
+    @JoinColumn(name = "article_id")  // article_id라는 이름으로 Column이 생성된다.
+    private Article article;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String body;
 }
--- springboot_example/src/main/resources/data.sql
+++ springboot_example/src/main/resources/data.sql
@@ -1,3 +1,22 @@
 INSERT INTO article(id, title, content) values (1, '가가가가', '1111');
 INSERT INTO article(id, title, content) values (2, '나나나나', '2222');
 INSERT INTO article(id, title, content) values (3, '다다다다', '3333');
+
+
+ -- article 더미 데이터 추가
+INSERT INTO article(id, title, content) values (4, '당신의 인생 영화는?', '댓글 ㄱ');
+INSERT INTO article(id, title, content) values (5, '당신의 소울 푸드는?', '댓글 ㄱㄱ');
+INSERT INTO article(id, title, content) values (6, '당신의 취미는?', '댓글 ㄱㄱㄱ');
+
+--comment 더미 데이터
+INSERT INTO comment(id, article_id, nickname, body) values (1, 4, 'Park', '굳 윌 헌팅');
+INSERT INTO comment(id, article_id, nickname, body) values (2, 4, 'Kim', '아이 엠 샘');
+INSERT INTO comment(id, article_id, nickname, body) values (3, 4, 'Choi', '쇼생크의 탈출');
+
+INSERT INTO comment(id, article_id, nickname, body) values (4, 5, 'Park', '치킨');
+INSERT INTO comment(id, article_id, nickname, body) values (5, 5, 'Kim', '샤브샤브');
+INSERT INTO comment(id, article_id, nickname, body) values (6, 5, 'Choi', '초밥');
+
+INSERT INTO comment(id, article_id, nickname, body) values (7, 6, 'Park', '조깅');
+INSERT INTO comment(id, article_id, nickname, body) values (8, 6, 'Kim', '유튜브');
+INSERT INTO comment(id, article_id, nickname, body) values (9, 6, 'Choi', '독서');
